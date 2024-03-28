63f8d2c2b6d493a15eb623d9c7ce315ba6007fe7
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-03-28 08:55:37 +0300

    preorder traversal

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,12 +6,12 @@
  * Return: 1 if node is a root, 0 otherwise
  */
 
- int binary_tree_is_root(const binary_tree_t *node)
- {
+int binary_tree_is_root(const binary_tree_t *node)
+{
 	if (node == NULL)
 		return (0);
 	if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
- }
+}
