92dc8e1409f40241d7a2352754b4cf811ca9834c
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-03-28 21:10:21 +0300

    measures the balance factor of BT

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: balance factor of the tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+	int leftheight = 0;
+	int rightheight = 0;
+
+	if (tree->left)
+		leftheight = 1 + binary_tree_balance(tree->left);
+	if (tree->right)
+		rightheight = 1 + binary_tree_balance(tree->right);
+
+	return (leftheight - rightheight);
+	}
+	else
+		return (0);
+}
+
